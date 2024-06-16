a87224bd004128f742e5c78e2e34b7f7bf62e966
Ghozi Izzulhaq <ghozihaq2017@gmail.com>
2024-06-16 17:10:43 +0700

    feat: Set up bottom tab navigation

---
+++ src/screens/Favorite.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+function Favorite() {
+  return (
+    <View>
+      <Text>Favorite</Text>
+    </View>
+  )
+}
+
+export default Favorite;
